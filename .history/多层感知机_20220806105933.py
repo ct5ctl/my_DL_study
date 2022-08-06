33b6ccf516a5d81d79dfa7aec538671e78c531e1
ct5ctl <1079314004@qq.com>
2022-08-06 11:34:41 +0800

    change README.md

--- .history/多层感知机_20220806105933.py
+++ .history/多层感知机_20220806105933.py
@@ -2,10 +2,6 @@ import torch
 from torch import nn
 from d2l import torch as d2l
 
-def init_weights(m):
-    if type(m) == nn.Linear:
-        nn.init.normal_(m.weight, std=0.01) # std是标准差
-
 if __name__ == '__main__':
     # 超参数
     batch_size = 64
@@ -14,6 +10,9 @@ if __name__ == '__main__':
     train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
     # 定义模型及参数
     net = nn.Sequential(nn.Flatten(), nn.Linear(28*28, 256), nn.ReLU(), nn.Linear(256, 10))
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.normal_(m.weight, std=0.01) # std是标准差
     net.apply(init_weights)
     # 选择损失函数
     loss = nn.CrossEntropyLoss(reduction="none")    # reduction="none"即要求求出的损失不要求均值
